1c821ad932fd971eb0a13602bc8264643164e85a
AnjeshDbz <anjesh51227@gmail.com>
2025-01-15 16:22:08 +0530

    checkTokenMiddleware

--- express-project/index.js
+++ express-project/index.js
@@ -1,51 +1,52 @@
 let express = require("express");
+const { checkToken } = require("./checkTokenMiddleware");
 
 let app = express();
 app.use(express.json());
-let myToken = "12345";
+// let myToken = "12345";
 let myPass = "12345";
 
-let checkToken = (req, res, next) => {
-  console.log(req.query.token);
+// let checkToken = (req, res, next) => {
+//   console.log(req.query.token);
 
-  if (req.query.token == " " || req.query.token == undefined) {
-    return res.send({
-      status: 0,
-      msg: "please fill the token",
-    });
-  }
-  if (req.query.token != myToken) {
-    return res.send({
-      status: 0,
-      msg: "please fill the corect token",
-    });
-  }
-  next();
-};
+//   if (req.query.token == " " || req.query.token == undefined) {
+//     return res.send({
+//       status: 0,
+//       msg: "please fill the token",
+//     });
+//   }
+//   if (req.query.token != myToken) {
+//     return res.send({
+//       status: 0,
+//       msg: "please fill the corect token",
+//     });
+//   }
+//   next();
+// };
 
-app.use(checkToken); //Middleware
+// app.use(checkToken); //Middleware
 
-app.use((req, res, next) => {
-  if (req.query.pass == " " || req.query.pass == undefined) {
-    return res.send({
-      status: 0,
-      msg: "please fill the password",
-    });
-  }
-  if (req.query.pass != myPass) {
-    return res.send({
-      status: 0,
-      msg: "please fill the corect password",
-    });
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   if (req.query.pass == " " || req.query.pass == undefined) {
+//     return res.send({
+//       status: 0,
+//       msg: "please fill the password",
+//     });
+//   }
+//   if (req.query.pass != myPass) {
+//     return res.send({
+//       status: 0,
+//       msg: "please fill the corect password",
+//     });
+//   }
+//   next();
+// });
 
 app.get("/", (req, res) => {
   res.send({ status: 1, msg: "Home page Api" });
 });
 
-app.get("/news", (req, res) => {
+app.get("/news", checkToken, (req, res) => {
   res.send({ status: 2, msg: "Nesw page Api" });
 });
 
